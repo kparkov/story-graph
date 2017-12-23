e65370022172fc89fdbd2456136785dd2f34a451
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2017-12-23 23:11:25 +0100

    Add Swashbuckle automated swagger definition

---
+++ StoryGraph/Api/Controllers/EntityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KVP.StoryGraph.Api.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Entity")]
+    public class EntityController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Entity> Get()
+        {
+            return new List<Entity>()
+            {
+                new Entity() { Id = "dsx", Title = "My thing", Description = "A thing indeed."}
+            };
+        }
+    }
+}
--- StoryGraph/Api/Startup.cs
+++ StoryGraph/Api/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace KVP.StoryGraph.Api
 {
@@ -18,6 +19,11 @@ namespace KVP.StoryGraph.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {Title = "My API", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +35,12 @@ namespace KVP.StoryGraph.Api
             }
 
             app.UseMvc();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
