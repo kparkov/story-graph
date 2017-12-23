00fe7563833d1a642c437bb48dbf1af6d7a03fa2
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2017-12-23 23:31:17 +0100

    INCREMENT

--- StoryGraph/Test/TestTests.cs
+++ StoryGraph/Test/TestTests.cs
@@ -9,7 +9,7 @@ namespace KVP.StoryGraph.Test
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(2, 1);
+            Assert.AreEqual(1, 1);
         }
 
         [TestMethod]
