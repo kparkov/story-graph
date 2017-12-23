7eb4e09ff0650eae6c3f69697a1909fb6729fb6d
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2017-12-23 21:09:47 +0100

    Add test project

---
+++ StoryGraph/Test/TestTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class TestTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
